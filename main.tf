a4354f1add9ec637343e9ec7ec15301ef07b18e1
Srinu779409 <168169931+Srinu779409@users.noreply.github.com>
2024-06-07 12:07:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-067aaeea6813afbde"
+    ami = "ami-00fa32593b478ad6e "
     instance_type = "t2.nano"
     tags = {
       Name = "dev-ec2"
